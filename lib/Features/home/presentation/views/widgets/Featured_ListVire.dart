2ae966176ef0a5aab089431133b4c25d1c892946
Ahmad <you@example.com>
2023-08-15 18:45:42 +0300

    create styles file and add title to Home View

---
+++ lib/Features/home/presentation/views/widgets/Featured_ListVire.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+
+import 'Custom_listView_item.dart';
+
+class FeaturedBooksListView extends StatelessWidget {
+  const FeaturedBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .3,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return const CustomListviewitem();
+        },
+      ),
+    );
+  }
+}
--- lib/Features/home/presentation/views/widgets/Home_View_body.dart
+++ lib/Features/home/presentation/views/widgets/Home_View_body.dart
@@ -1,31 +1,30 @@
+import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 import 'Custom_App_Bar.dart';
 import 'Custom_listView_item.dart';
+import 'Featured_ListVire.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [CustomAppBar(), FeaturedBooksListView()],
-    );
-  }
-}
-
-class FeaturedBooksListView extends StatelessWidget {
-  const FeaturedBooksListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-          return const CustomListviewitem();
-        },
+    return const Padding(
+      padding: EdgeInsets.only(left: 24),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(),
+          FeaturedBooksListView(),
+          SizedBox(
+            height: 24,
+          ),
+          Text(
+            "Best Salery",
+            style: Styles.titleMidium,
+          )
+        ],
       ),
     );
   }

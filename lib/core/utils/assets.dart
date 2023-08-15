db1601c6a20b98f300c76a23d4357ca15699d317
Ahmad <you@example.com>
2023-08-15 17:19:57 +0300

    ..

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,4 +1,5 @@
 class AssetsData {
   static const logo = 'assets/images/Logo.png';
   static const iconSearch = 'assets/images/Scroll Group 1.png';
+  static const testimage = 'assets/images/test_image.png';
 }

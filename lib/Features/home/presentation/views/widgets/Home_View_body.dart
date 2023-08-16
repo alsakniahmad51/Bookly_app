ca7a15ac419d9274dcef3393bf20abfa902f78e8
Ahmad <you@example.com>
2023-08-16 17:29:21 +0300

    best seller list View Item

--- lib/Features/home/presentation/views/widgets/Home_View_body.dart
+++ lib/Features/home/presentation/views/widgets/Home_View_body.dart
@@ -1,6 +1,7 @@
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../../core/utils/assets.dart';
 import 'Custom_App_Bar.dart';
 import 'Featured_ListVire.dart';
 
@@ -22,8 +23,38 @@ class HomeViewBody extends StatelessWidget {
           Text(
             "Best Salery",
             style: Styles.titleMidium,
-          )
+          ),
+          BestSellerListViewItem()
         ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20),
+      child: SizedBox(
+        height: 150,
+        child: Row(
+          children: [
+            AspectRatio(
+              aspectRatio: 2.5 / 4,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  image: const DecorationImage(
+                    image: AssetImage(AssetsData.testimage),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }

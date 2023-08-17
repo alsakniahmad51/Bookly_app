61ab615fa83dc6d918d6f724345bdbda7172f6db
Ahmad <you@example.com>
2023-08-17 15:56:02 +0300

    Best Seller item part 3

--- lib/Features/home/presentation/views/widgets/Home_View_body.dart
+++ lib/Features/home/presentation/views/widgets/Home_View_body.dart
@@ -1,9 +1,9 @@
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
-import '../../../../../core/utils/assets.dart';
 import 'Custom_App_Bar.dart';
 import 'Featured_ListVire.dart';
+import 'best_seller_listView_Item.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -26,48 +26,6 @@ class HomeViewBody extends StatelessWidget {
           ),
           BestSellerListViewItem()
         ],
-      ),
-    );
-  }
-}
-
-class BestSellerListViewItem extends StatelessWidget {
-  const BestSellerListViewItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 20),
-      child: SizedBox(
-        height: 150,
-        child: Row(
-          children: [
-            AspectRatio(
-              aspectRatio: 2.8 / 4,
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                  image: const DecorationImage(
-                    image: AssetImage(AssetsData.testimage),
-                  ),
-                ),
-              ),
-            ),
-            Column(
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * .5,
-                  child: const Text(
-                    'Harry Botter and the Goblet of Fire',
-                    style: Styles.textStyle20,
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                )
-              ],
-            )
-          ],
-        ),
       ),
     );
   }
---
+++ lib/Features/home/presentation/views/widgets/best_seller_listView_Item.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/cupertino.dart';
+
+import '../../../../../constants.dart';
+import '../../../../../core/utils/assets.dart';
+import '../../../../../core/utils/styles.dart';
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20),
+      child: SizedBox(
+        height: 150,
+        child: Row(
+          children: [
+            AspectRatio(
+              aspectRatio: 2.8 / 4,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  image: const DecorationImage(
+                    image: AssetImage(AssetsData.testimage),
+                  ),
+                ),
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * .5,
+                  child: Text(
+                    'Harry Botter and the Goblet of Fire',
+                    style:
+                        Styles.textStyle20.copyWith(fontFamily: kGtSectraFine),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                const SizedBox(
+                  height: 3,
+                ),
+                const Text(
+                  "J.K. Rowling",
+                  style: Styles.textStyle14,
+                ),
+                const Row(
+                  children: [
+                    SizedBox(
+                      height: 3,
+                    ),
+                  ],
+                ),
+                Text(
+                  "19.99 €",
+                  style:
+                      Styles.textStyle20.copyWith(fontWeight: FontWeight.bold),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}

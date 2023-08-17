283254727c3a7e66c6be065a753dcb7bc2e928e3
Ahmad <you@example.com>
2023-08-17 17:04:35 +0300

    ....

--- lib/Features/home/presentation/views/widgets/best_seller_listView_Item.dart
+++ lib/Features/home/presentation/views/widgets/best_seller_listView_Item.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/cupertino.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
 import '../../../../../constants.dart';
 import '../../../../../core/utils/assets.dart';
 import '../../../../../core/utils/styles.dart';
